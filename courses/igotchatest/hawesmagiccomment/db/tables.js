dc9c855d6276ae18c14cf165f0d6325f2377e49a
terence1991 <cplterencehawes@gmail.com>
2019-07-23 16:16:41 -0400

    express/knex implement with mysql

---
+++ courses/igotchatest/hawesmagiccomment/db/tables.js
@@ -0,0 +1,23 @@
+
+//connecttodb 
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    filename: './index.js',
+  },
+});
+
+
+knex.schema('users', (table) => {
+  table.increments('id');
+  table.string('user_name');
+});
+
+knex.schema('comments', (table) => {
+  table.increments('id');
+  table.string('user_name');
+  table.string('comments');
+  table.integer('user_rating');
+})
+
+
