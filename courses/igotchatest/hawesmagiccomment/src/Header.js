059617d1791a65eb920bbc46e3c7faf5000c373b
terence1991 <cplterencehawes@gmail.com>
2019-07-23 10:24:15 -0400

    header added

---
+++ courses/igotchatest/hawesmagiccomment/src/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button'
+
+
+export class TopBar extends React.Component {
+  render() {
+      return (
+          <Header onClick={this.props.refershteamselection}>
+              <Title>
+              Hawes Magic Team
+              </Title>
+              <Button >Submit Comment</Button>
+              {/* {this.props.displayErrorMessage && <span>{this.props.children}</span>} */}
+          </Header>
+      )
+  }
+
+}
+
+export default TopBar;
+
+const Header = styled.header`
+line-height: 60px;
+font-weight: 400;
+box-shadow: 0 1px 3px 0 #5E35B1;
+height: 60px;
+background-color: #7E57C2;
+top: 0;
+left: 0;
+position: fixed;
+right: 0;
+z-index: 10000;
+`
+
+const Title = styled.span`
+color: #fff;
+height: 60px;
+float: left;
+margin-right: 30px;
+margin-top: 0;
+font-size: 24px;
+font-family: 'Coiny', sans-serif;
+font-variant: small-caps;
+`
